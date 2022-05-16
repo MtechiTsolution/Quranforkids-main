0e1204faee3c5bebf30a113d47cca1e58aec1391
Mujahid Hussain <errorbroken.cs@gmail.com>
2022-05-16 00:19:06 -0700

    surah
    mukaml ho gia ha read vala denting painting kr dana

--- app/src/main/java/com/example/localdata/alldata_surah.java
+++ app/src/main/java/com/example/localdata/alldata_surah.java
@@ -1,4 +1,5 @@
 package com.example.localdata;
+import com.Models.Aya_Model;
 import com.Models.Dua_model;
 import com.Models.Surah_model;
 import com.example.qfk.R;
@@ -7,19 +8,24 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class alldata_surah {
+    public static final ArrayList<Aya_Model> surahnas=new ArrayList<>(Arrays.asList(
+            new Aya_Model("(1)أَلَمْ تَرَ كَيْفَ فَعَلَ رَبُّكَ بِأَصْحَـٰبِ ٱلْفِيل","کیا تم نے نہیں دیکھا کہ تمہارے پروردگار نے ہاتھی والوں کے ساتھ کیا کیا","Have you not seen ˹O Prophet˺ how your Lord dealt with the Army of the Elephant?"),new Aya_Model("arbi","urdu","english")));
+    public static final ArrayList<Aya_Model> surahfalaq=new ArrayList<>(Arrays.asList(
+            new Aya_Model("arbi2","urdu","english"),new Aya_Model("arbi2","urdu","english")));
+
     public static final ArrayList<Surah_model> surah_models=
             new ArrayList<>(Arrays.asList(
                     new Surah_model("Surah An-Nas","1","سُوْرَۃُ النَّاس",
                             "سُوْرَۃُ النَّاس "
                             ,"(1)   قُلۡ اَعُوۡذُ بِرَبِّ النَّاسِۙ",
                             "Say: I seek refuge in the Lord of mankind,",
-                            "کہو کہ میں لوگوں کے پروردگار کی پناہ مانگتا ہوں"),
+                            "کہو کہ میں لوگوں کے پروردگار کی پناہ مانگتا ہوں",surahnas),
                     new Surah_model("Surah An-Nas","2","سُوْرَۃُ النَّاس","سوتے وقت ", "اَللّٰھُمَّ بِاسْمِکَ  اَمُوْتُ وَاَحْیَا",
                             "O Allah (Almighty) I live and die in your name.",
-                            "الٰہی عزوجل میں تیرے نام پر مرتا ہوں اور جیتا ہوں۔"),
+                            "الٰہی عزوجل میں تیرے نام پر مرتا ہوں اور جیتا ہوں۔",surahfalaq),
                     new Surah_model("Surah An-Nas","3","سُوْرَۃُ النَّاس","سوتے وقت ", "اَللّٰھُمَّ بِاسْمِکَ  اَمُوْتُ وَاَحْیَا",
                             "O Allah (Almighty) I live and die in your name.",
-                            "الٰہی عزوجل میں تیرے نام پر مرتا ہوں اور جیتا ہوں۔")
+                            "الٰہی عزوجل میں تیرے نام پر مرتا ہوں اور جیتا ہوں۔",surahnas)
                     ));
 }
 
--- app/src/main/java/com/example/qfk/Surah_1.java
+++ app/src/main/java/com/example/qfk/Surah_1.java
@@ -3,19 +3,27 @@ package com.example.qfk;
 import static com.example.localdata.alldata_surah.surah_models;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.Models.Aya_Model;
 import com.Models.Dua_model;
 import com.Models.Surah_model;
 import com.example.localdata.alldata;
 import com.example.localdata.alldata_surah;
 
+import Adopters.ayatAdapter;
+import Adopters.myAdapter2_surah;
+
 public class Surah_1 extends AppCompatActivity {
     TextView title,arabic,u_trans,e_trans;
+    RecyclerView rec;
    // ImageView imageView;
     int posotion;
     @Override
@@ -23,10 +31,12 @@ public class Surah_1 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_surah1);
         getSupportActionBar().hide();
+        rec=findViewById(R.id.surah_adapter);
         title=findViewById(R.id.title2);
         arabic=findViewById(R.id.arabic);
         u_trans=findViewById(R.id.u_trans);
         e_trans=findViewById(R.id.e_trans);
+
        // imageView=findViewById(R.id.image);
 
 
@@ -35,10 +45,15 @@ public class Surah_1 extends AppCompatActivity {
       //  Surah_model surah_model= alldata.surah_models.get(intValue);
             Surah_model surah_model = surah_models.get(intValue);
 
-        title.setText(surah_model.title22);
-        arabic.setText(surah_model.arabicc);
-        e_trans.setText(surah_model.e_transalationn);
-        u_trans.setText(surah_model.u_translationn);
+        rec.setLayoutManager(new LinearLayoutManager(this));
+        Toast.makeText(Surah_1.this, ""+surah_model.ayats.size(), Toast.LENGTH_SHORT).show();
+        ayatAdapter ayatAdapter=new ayatAdapter(this, surah_model.ayats);
+        rec.setAdapter(ayatAdapter);
+//
+        title.setText(surah_model.titile);
+//        arabic.setText(surah_model.arabicc);
+//        e_trans.setText(surah_model.e_transalationn);
+//        u_trans.setText(surah_model.u_translationn);
        // imageView.setImageResource(dua_model.image);
 
 
