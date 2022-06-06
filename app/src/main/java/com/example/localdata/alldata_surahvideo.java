ce6d40d39d77c6ecb1f453021044c2fdd898f55c
shamsachuri <shamsazia1999@gmail.com>
2022-06-06 00:24:20 -0700

    Update alldata_surahvideo.java
    .

--- app/src/main/java/com/example/localdata/alldata_surahvideo.java
+++ app/src/main/java/com/example/localdata/alldata_surahvideo.java
@@ -12,14 +12,41 @@ import java.util.Arrays;
 public class alldata_surahvideo {
     public static final ArrayList<surah_video_model> video_models=
             new ArrayList<>(Arrays.asList(
-                    new surah_video_model(R.drawable.listen_surah_pic,"Surah video 1","Surah anas"),
-                    new surah_video_model(R.drawable.listen_surah_pic,"Surah video 2","Surah anas"),
-                    new surah_video_model(R.drawable.listen_surah_pic,"Surah video 3","Surah anas"),
-                    new surah_video_model(R.drawable.listen_surah_pic,"Surah video 4","Surah anas"),
-                    new surah_video_model(R.drawable.listen_surah_pic,"Surah video 5","Surah anas"),
-                    new surah_video_model(R.drawable.listen_surah_pic,"Surah video 6","Surah anas")
-
-
+                    new surah_video_model(R.drawable.listen_surah_pic,"Surah An-Nas","سُوْرَۃُ النَّاس"),
+                    new surah_video_model(R.drawable.listen_surah_pic,"Surah Al-Falaq","سُوْرَۃُ الفَلَق"),
+                    new surah_video_model(R.drawable.listen_surah_pic,"Surah Al-Ikhlas","سُوْرَۃُ الإِخْلَاص"),
+                    new surah_video_model(R.drawable.listen_surah_pic,"Surah Al-Masad","سُوْرَۃُ المَسَد"),
+                    new surah_video_model(R.drawable.listen_surah_pic,"Surah An-Nasr","سُوْرَۃُ النَّصْر"),
+                    new surah_video_model(R.drawable.listen_surah_pic,"Surah Al-Kafirun","سُوْرَۃُ الكَافِرُون"),
+                    new surah_video_model(R.drawable.listen_surah_pic,"Surah Al-Kawthar","سُوْرَۃُ الكَوْثَر"),
+                    new surah_video_model(R.drawable.listen_surah_pic,"Surah Al-Ma'un","سُوْرَۃُ المَاعُون"),
+                    new surah_video_model(R.drawable.listen_surah_pic,"Surah Quraysh","سُوْرَۃ قُرَيْش"),
+                    new surah_video_model(R.drawable.listen_surah_pic,"Surah Al-Fil","سُوْرَۃُ الفِيل"),
+                    new surah_video_model(R.drawable.listen_surah_pic,"Surah Al-Humazah","سُوْرَۃُ الهُمَزَة"),
+                    new surah_video_model(R.drawable.listen_surah_pic,"Surah Al-'Asr","سُوْرَۃُ العَصْر"),
+                    new surah_video_model(R.drawable.listen_surah_pic,"Surah At-Takathur","سُوْرَۃُ التَّكَاثُر"),
+                    new surah_video_model(R.drawable.listen_surah_pic,"Surah Al-Qari'ah","سُوْرَۃُ القَارِعَة"),
+                    new surah_video_model(R.drawable.listen_surah_pic,"Surah Al-'Adiyat","سُوْرَۃُ العَادِيَات"),
+                    new surah_video_model(R.drawable.listen_surah_pic,"Surah Az-Zalzalah","سُوْرَۃُ الزَّلْزَلَة"),
+                    new surah_video_model(R.drawable.listen_surah_pic,"Surah Al-Bayyinah","سُوْرَۃُ البَيِّنَة"),
+                    new surah_video_model(R.drawable.listen_surah_pic,"Surah Al-Qadr","سُوْرَۃُ القَدْر"),
+                    new surah_video_model(R.drawable.listen_surah_pic,"Surah Al-'Alaq","سُوْرَۃُ العَلَق"),
+                    new surah_video_model(R.drawable.listen_surah_pic,"Surah At-Tin","سُوْرَۃُ التِّين"),
+                    new surah_video_model(R.drawable.listen_surah_pic,"Surah Ash-Sharh","سُوْرَۃُ الشَّرْح"),
+                    new surah_video_model(R.drawable.listen_surah_pic,"Surah Ad-Duhaa","سُوْرَۃُ الضُّحَى"),
+                    new surah_video_model(R.drawable.listen_surah_pic,"Surah Al-Layl","سُوْرَۃُ اللَّيْل"),
+                    new surah_video_model(R.drawable.listen_surah_pic,"Surah Ash-Shams","سُوْرَۃُ الشَّمْس"),
+                    new surah_video_model(R.drawable.listen_surah_pic,"Surah Al-Balad","سُوْرَۃُ البَلَدٰ"),
+                    new surah_video_model(R.drawable.listen_surah_pic,"Surah Al-Fajr","سُوْرَۃُ الفَجْر"),
+                    new surah_video_model(R.drawable.listen_surah_pic,"Surah Al-Ghashiyah","سُوْرَۃُ الغَاشِيَة"),
+                    new surah_video_model(R.drawable.listen_surah_pic,"Surah Al-A'la","سُوْرَۃُ الأَعْلَىٰ"),
+                    new surah_video_model(R.drawable.listen_surah_pic,"Surah At-Tariq","سُوْرَۃُ الطَّارِق"),
+                    new surah_video_model(R.drawable.listen_surah_pic,"Surah Al-Buruj","سُوْرَۃُ البُرُوج"),
+                    new surah_video_model(R.drawable.listen_surah_pic,"Surah Al-Inshiqaq","سُوْرَۃُ الإِنْشِقَاق"),
+                    new surah_video_model(R.drawable.listen_surah_pic,"Surah Al-Mutaffifin","سُوْرَۃُ المُطَفِّفِين"),
+                    new surah_video_model(R.drawable.listen_surah_pic,"Surah Al-Infitar","سُوْرَۃُ الإِنْفِطَار"),
+                    new surah_video_model(R.drawable.listen_surah_pic,"Surah At-Takwir","سُوْرَۃُ التَّكْوِير"),
+                    new surah_video_model(R.drawable.listen_surah_pic,"Surah Fatiha","سُوْرَۃُ الفَاتِحَة")
                     ));
 
 
